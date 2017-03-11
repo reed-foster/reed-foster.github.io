91e336d7f86e3b93b3a5717c1f5b9c730bb5b32c
reed-foster <reed.a.foster@gmail.com>
2017-03-10 20:53:04 -0800

    added zooming functionality

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,7 +1,6 @@
-﻿function map(x, x0, y0, x1, y1)
+﻿function map(x, in_min, in_max, out_min, out_max)
 {
-	//f(x) = (y1 - y0)/(x1 - x0) * (x - x0) + y0
-	return (y1 - y0) / (x1 - x0) * (x - x0) + y0;
+	return (out_max - out_min) / (in_max - in_min) * (x - in_min) + out_min;
 }
 
 function mandelbrot(canvas, xmin, xmax, ymin, ymax, iterations)
@@ -9,8 +8,10 @@ function mandelbrot(canvas, xmin, xmax, ymin, ymax, iterations)
 	var palette = []
 	for (var i = 0; i <= iterations; i++)
 	{
-		palette.push([i / iterations * 255, 0, map(i, 0, 128, iterations, 0)])
+		palette.push([map(i, 0, iterations, 0, 10), map(i, 0, iterations, 75, 255), map(i, 0, iterations, 0, 70)])
 	}
+	palette.push([0, 0, 0]);
+	
 	var width = canvas.width;
 	var height = canvas.height;
 	
@@ -27,76 +28,65 @@ function mandelbrot(canvas, xmin, xmax, ymin, ymax, iterations)
 			var iteration = 0;
 			var max_iteration = 1000;
 			
-			var pix_pos = 4 * (width * y0 + x0);
+			var pix_pos = 4 * (width * iy + ix);
 			
-			while ( x*x + y*y < (1 << 8)  &&  iteration < iterations)
+			while (x*x + y*y < 4  &&  iteration <= iterations)
 			{
 				xtemp = x*x - y*y + x0;
 				y = 2*x*y + y0;
 				x = xtemp;
 				iteration++;
-			}		
-			// Used to avoid floating point issues with points inside the set.
-			if (iteration < iterations) {
-				// sqrt of inner term removed using log simplification rules.
-				var log_zn = Math.log( x*x + y*y ) / 2;
-				var nu = Math.log(log_zn / Math.log(2)) / Math.log(2);
-				// Rearranging the potential function.
-				// Dividing log_zn by log(2) instead of log(N = 1<<8)
-				// because we want the entire palette to range from the
-				// center to radius 2, NOT our bailout radius.
-				iteration = iteration + 1 - nu;
 			}
-			//color1 = palette[floor(iteration)]
-			//color2 = palette[floor(iteration) + 1]
-			//color = linear_interpolate(color1, color2, iteration % 1)
-			//plot(Px, Py, color)
-			color = palette[Math.floor(iteration)];
-			pix[pix_pos] = color[0];
-			pix[pix_pos + 1] = color[1];
-			pix[pix_pos + 2] = color[2];
+			if (iteration == iterations)
+			{
+				pix[pix_pos] = 0;
+				pix[pix_pos + 1] = 0;
+				pix[pix_pos + 2] = 0;
+			}
+			else
+			{
+				color = palette[Math.floor(iteration)];
+				pix[pix_pos] = color[0];
+				pix[pix_pos + 1] = color[1];
+				pix[pix_pos + 2] = color[2];
+			}
 			pix[pix_pos + 3] = 255;
 		}
 		
 	}
-	/*
-      var x = xmin + (xmax - xmin) * ix / (width - 1);
-      var y = ymin + (ymax - ymin) * iy / (height - 1);
-      var i = mandelIter(x, y, iterations);
-      var ppos = 4 * (width * iy + ix);
- 
-      if (i > iterations) {
-        pix[ppos] = 0;
-        pix[ppos + 1] = 0;
-        pix[ppos + 2] = 0;
-      } else {
-        var c = 3 * Math.log(i) / Math.log(iterations - 1.0);
- 
-        if (c < 1) {
-          pix[ppos] = 255 * c;
-          pix[ppos + 1] = 0;
-          pix[ppos + 2] = 0;
-        }
-        else if ( c < 2 ) {
-          pix[ppos] = 255;
-          pix[ppos + 1] = 255 * (c - 1);
-          pix[ppos + 2] = 0;
-        } else {
-          pix[ppos] = 255;
-          pix[ppos + 1] = 255;
-          pix[ppos + 2] = 255 * (c - 2);
-        }
-      }
-      pix[ppos + 3] = 255;
-    }*/
- 
-  ctx.putImageData(img, 0, 0);
+	ctx.putImageData(img, 0, 0);
 }
  
-var canvas = document.createElement('canvas');
-canvas.width = 800;
-canvas.height = 500;
- 
+var canvas = document.getElementById('MandelbrotCanvas');
 document.body.insertBefore(canvas, document.body.childNodes[0]);
+
+var xmin = 0, ymin = 0, xmax = canvas.width, ymax = canvas.height;
+var m_last_xmin = -2, m_last_ymin = -1, m_last_xmax = 1, m_last_ymax = 1;
+
+canvas.addEventListener('dblclick', function(event) {xmin = 0; ymin = 0; xmax = canvas.width; ymax = canvas.height; m_last_xmin = -2; m_last_ymin = -1; m_last_xmax = 1; m_last_ymax = 1;}, false);
+canvas.addEventListener('mousedown', function(event) {xmin = event.pageX; ymin = event.pageY;}, false);
+canvas.addEventListener('mouseup', function(event)
+{
+	xmax = event.pageX;
+	ymax = event.pageY;
+	if (xmin > xmax)
+	{
+		var tmp = xmin;
+		xmin = xmax;
+		xmax = tmp;
+	}
+	if (ymin > ymax)
+	{
+		var tmp = ymin;
+		ymin = ymax;
+		ymax = tmp;
+	}
+	m_xmin = map(xmin, 0, canvas.width, m_last_xmin, m_last_xmax);
+	m_xmax = map(xmax, 0, canvas.width, m_last_xmin, m_last_xmax);
+	m_ymin = map(ymin, 0, canvas.height, m_last_ymin, m_last_ymax);
+	m_ymax = map(ymax, 0, canvas.height, m_last_ymin, m_last_ymax);
+	mandelbrot(canvas, m_xmin, m_xmax, m_ymin, m_ymax, 3 * 25 * 1 / (m_xmax - m_xmin));
+	m_last_xmin = m_xmin; m_last_xmax = m_xmax; m_last_ymin = m_ymin; m_last_ymax = m_ymax;
+}, false);
  
-mandelbrot(canvas, -2, 1, -1, 1, 1000);
+mandelbrot(canvas, -2, 1, -1, 1, 50);
